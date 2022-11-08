2bfd9d0bdbf1ea146d8507cf97b9fc7e51d4045e
Trung <kientrung2002.kn@gmail.com>
2022-11-08 22:19:13 +0700

    fix all

--- models/crud.js
+++ models/crud.js
@@ -5,7 +5,7 @@ async function crud(req_body, session) {
     let product_name = req_body.name;
     let price = req_body.price;
     let quantity = req_body.quantity;
-    let shop_id = req_body.shop_id;
+    let shop_id = session.shop_id;
     if (req_body.crud == 'update') {
         var query = {
             text: `UPDATE products
@@ -26,10 +26,9 @@ async function crud(req_body, session) {
             values: [id, product_name, price, quantity, shop_id]
         }
     }
-
     results = await pg_conn.query(query);
 
     return results;
 }
 
-module.exports = crud
+module.exports = crud;
