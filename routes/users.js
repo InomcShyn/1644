31419d28cf6d69655782ef994154f0f561711bdc
Trung <kientrung2002.kn@gmail.com>
2022-11-09 08:26:59 +0700

    fix test

--- routes/users.js
+++ routes/users.js
@@ -1,43 +1,36 @@
-var express = require("express");
-const crud = require("../models/crud");
-const display_products = require("../models/product_table");
+var express = require('express');
 var router = express.Router();
+var display_product = require('../models/product_table');
+var crud = require('../models/crud');
 var session;
 
 /* GET users listing. */
-router.get("/", async(req, res, next) => {
+router.get('/', async function(req, res, next) {
     session = req.session;
     if (session.user_id) {
         let username = session.user_id;
         let shop_id = session.shop_id;
-        let table = await display_products(shop_id);
-
-        res.render("users", {
-            title: "Store owner ATN shop",
+        let table = await display_product(shop_id, session);
+        res.render('users', {
+            title: 'ATN Shop',
             name: username,
-            table_string: table,
-        });
+            table_string: table
+        })
     } else {
-        res.render("login", { title: "ATN SHOP", message: "Please login" });
+        res.redirect('/login');
     }
 });
 
-router.post("/crud", async(req, res, next) => {
-    try {
-        session = req.session;
-        console.log(req.body);
-        let results = await crud(req.body);
-
-        // refresh the page
-        let table = await display_products(req.body.shop_id);
-        res.render("users", {
-            title: "Welcome user to ATN shop",
-            name: session.user_id,
-            table_string: table,
-        });
-    } catch (error) {
-        console.error(error.message);
-    }
+router.post('/crud', async function(req, res, next) {
+    console.log(req.body);
+    let results = await crud(req.body, session);
+    //refresh the page
+    let table = await display_product(session.shop_id, session);
+    res.render('users', {
+        title: 'ATN Shop',
+        name: session.user_id,
+        table_string: table
+    })
 });
 
 module.exports = router;
