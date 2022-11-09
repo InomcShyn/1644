42276b297b1f56e38188dae3eee777bceb3c0aae
Trung <kientrung2002.kn@gmail.com>
2022-11-09 08:23:18 +0700

    test 1

--- routes/admin.js
+++ routes/admin.js
@@ -4,34 +4,38 @@ var display_product = require('../models/product_table');
 const { render } = require('ejs');
 var router = express.Router();
 
-/* GET home page. */
 var session;
-router.get('/', async function(req, res, next) {
+/* GET admin page. */
+router.get("/", async(req, res, next) => {
     session = req.session;
-    console.log(session.shop_id)
-    if (session.user_id) {
-        let shop_id = session.shop_id;
-        let username = session.user_id;
-        let table = await display_product(shop_id, session);
-        let box_string = await gen_box();
-        res.render('admin', { title: 'Admin Page', name: username, select_box: box_string, table_string: table })
-    } else {
-        res.redirect('/login');
-    }
+    let shop_id = session.shop_id;
+    let username = session.user_id;
+
+    // calling display products
+    let table = await display_products(shop_id);
+    // select box string
+    let select_box = await gen_box();
+
+    res.render("admin", {
+        title: "Admin",
+        name: username,
+        table_str: table,
+        select_box_str: select_box,
+    });
 });
 
-router.post('/select_box', async function(req, res, next) {
-    let shop_id = req.body.shop;
-    username = req.session.user_id;
-    let table = await display_product(shop_id, session);
-    let box_string = await gen_box(shop_id);
-    res.render('admin', {
-        title: 'Admin Page',
+router.post("/select_box", async(req, res, next) => {
+    let shop_id = req.body.shops;
+    console.log(req.body.shops);
+    let username = req.session.user_id;
+    let table = await display_products(shop_id);
+    let box_str = await gen_box();
+    res.render("admin", {
+        title: "Admin",
         name: username,
-        select_box: box_string,
-        table_string: table
+        select_box_str: box_str,
+        table_str: table,
     });
-
 });
 
 module.exports = router;
